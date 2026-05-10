a9db29e7ff787fab52ad137e7a23a4331e858036
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:13:12 +0200

    Update solution.py
    Week 7 - Python Individual Assignment
    Student: Seid Mamuti
    project2 number guessing game/solution.py

--- projects/project2-number-guessing-game/solution.py
+++ projects/project2-number-guessing-game/solution.py
@@ -14,3 +14,21 @@ import random
 #   - count each guess
 
 # TODO: print the congratulations message with the number of guesses
+# Project 2 - Number Guessing Game
+# Author: Seid Mamuti
+import random
+# Pick a random number between 1 and 10
+secret = random.randint(1, 10)
+# Counter for number of guesses
+guesses = 0
+# Ask the user for the first guess
+guess = int(input("Guess a number between 1 and 10: "))
+guesses += 1
+# Repeat while the guess is not correct
+while guess != secret:
+if guess < secret:
+guess = int(input("Too low! Try again: "))
+else:
+guess = int(input("Too high! Try again: "))
+guesses += 1
+print(f"Correct! You got it in {guesses} guesses.")
